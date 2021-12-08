758a15288d826ccf850e5934582bc4b398a87ffb
meisn <meissner.ronny@gmail.com>
2021-12-08 20:36:11 +0100

    day8 pt1

--- day8/day8.py
+++ day8/day8.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 SEGMENTS = {0: set('abcefg'),
             1: set('cf'),
             2: set('acdeg'),
@@ -17,11 +19,25 @@ def read_input(file):
         for line in f:
             inraw, outraw = line.strip().split(" | ")
             inset = [set(s) for s in inraw.split() if s]
+            inlen = [len(s) for s in inraw.split() if s]
             outset = [set(s) for s in outraw.split() if s]
-            retval.append(dict(inraw=inraw, inset=inset,
-                          outraw=outraw, outset=outset))
+            outlen = [len(s) for s in outraw.split() if s]
+            retval.append(dict(inraw=inraw, inset=inset, inlen=inlen,
+                          outraw=outraw, outset=outset, outlen=outlen))
         return tuple(retval)
+
+
+def answer1(inputlist, searchlist):
+    res = defaultdict(int)
+    for lst in inputlist:
+        for nr in lst:
+            if nr in searchlist:
+                res[nr]+=1
+    return res
 
 
 if __name__ == "__main__":
     inp = read_input("2021_puzzle8.txt")
+    len1 = [i['outlen'] for i in inp]
+    ans1 = answer1(len1, [2,3,4,7])
+    print(f"Answer1: {sum(ans1.values())}")
